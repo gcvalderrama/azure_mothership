ee8e2371ddb9eb3c9864c357aca0e4dd8b2c57ac
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 20:33:04 -0500

    madd

--- production/build.ps1
+++ production/build.ps1
@@ -1,12 +1,12 @@
 # Install-Module -Name Az -AllowClobber -Scope CurrentUser
-Import-Module Az.Resources
+# Import-Module Az.Resources
 $params = @{
     vaults_secrets_name = "mothershipprodsecret"
     registries_registry_name = "mothershipprodregistry"
     userAssignedIdentities_identity_name = "mothershipappidentity"
 }
 
-Connect-AzAccount
+# Connect-AzAccount
 
 Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
 $resourceName = "mothership-rg"
