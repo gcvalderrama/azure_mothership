215a2e687b6a42539c1d7a38a3eb04447c95c7bc
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 20:19:24 -0500

    add

--- production/build.ps1
+++ production/build.ps1
@@ -1,3 +1,5 @@
+# Install-Module -Name Az -AllowClobber -Scope CurrentUser
+Import-Module Az.Resources
 $params = @{
     vaults_secrets_name = "mothershipprodsecret"
     registries_registry_name = "mothershipprodregistry"
@@ -10,3 +12,11 @@ Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
 $resourceName = "mothership-rg"
 $location = "eastus"
 New-AzResourceGroup -Name $resourceName -Location $location -Force
+
+New-AzResourceGroupDeployment `
+-Mode Complete `
+-Name mothershipProdDeployment `
+-ResourceGroupName $resourceName `
+-TemplateFile ./template/template.json `
+-TemplateParameterObject $params `
+-Force
