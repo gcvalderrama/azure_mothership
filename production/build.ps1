2db2146f2e62ffd50e66c1ed8afc62d33c720c57
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 20:03:13 -0500

    add

--- production/build.ps1
+++ production/build.ps1
@@ -10,12 +10,3 @@ Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
 $resourceName = "mothership-rg"
 $location = "eastus"
 New-AzResourceGroup -Name $resourceName -Location $location -Force
-
-New-AzResourceGroupDeployment `
--Mode Complete `
--Name OwlveyDeployment `
--ResourceGroupName $resourceName `
--TemplateFile ./../template.json `
--TemplateParameterObject $params `
--Force
-
