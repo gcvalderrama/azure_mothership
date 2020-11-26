8102c6cf78fdd889077e30d12249215e4662cc04
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-11-25 23:19:16 -0500

    ff

--- nonproduction/automate/build.ps1
+++ nonproduction/automate/build.ps1
@@ -1,9 +1,15 @@
-$clientId = ($env:credentials | ConvertFrom-Json).clientId
-$clientSecret = ($env:credentials | ConvertFrom-Json).clientSecret | ConvertTo-SecureString -AsPlainText -Force
-$tenantId = ($env:credentials | ConvertFrom-Json).tenantId
-$credentials = New-Object System.Management.Automation.PSCredential($clientId, $clientSecret)
+param ($debug = $false)
 
-$connected = Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+if ( $debug -ne $true ){
+    $clientId = ($env:credentials | ConvertFrom-Json).clientId
+    $clientSecret = ($env:credentials | ConvertFrom-Json).clientSecret | ConvertTo-SecureString -AsPlainText -Force
+    $tenantId = ($env:credentials | ConvertFrom-Json).tenantId
+    $credentials = New-Object System.Management.Automation.PSCredential($clientId, $clientSecret)    
+    $connected = Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+}
+else{
+    Connect-AzAccount
+}
 
 Set-AzContext -SubscriptionId "652129d9-5c47-49f8-ba58-0137a903edb0" -Force
 #
