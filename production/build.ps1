0f8918eaed9477eddf93f15e0688fd99285c57e7
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 20:57:34 -0500

    ccc

--- production/build.ps1
+++ production/build.ps1
@@ -6,7 +6,9 @@ $tenantId = ($env:credentials | ConvertFrom-Json).tenantId
 $credentials = New-Object System.Management.Automation.PSCredential($clientId, $clientSecret)
 
 $connected = Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
- 
+
+Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
+
 $params = @{
     vaults_secrets_name = "mothershipprodsecret"
     registries_registry_name = "mothershipprodregistry"
