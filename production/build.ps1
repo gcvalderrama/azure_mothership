ad0e17d9f1bffd7f8e16037dffe60d14cf305015
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 19:47:06 -0500

    add

--- production/build.ps1
+++ production/build.ps1
@@ -1,1 +1,21 @@
-write-host "test"
+$params = @{
+    vaults_secrets_name = "mothershipprodsecret"
+    registries_registry_name = "mothershipprodregistry"
+    userAssignedIdentities_identity_name = "mothershipappidentity"
+}
+
+Connect-AzAccount
+
+Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
+$resourceName = "mothership-rg"
+$location = "eastus"
+New-AzResourceGroup -Name $resourceName -Location $location -Force
+
+New-AzResourceGroupDeployment `
+-Mode Complete `
+-Name OwlveyDeployment `
+-ResourceGroupName $resourceName `
+-TemplateFile ./../template.json `
+-TemplateParameterObject $params `
+-Force
+
