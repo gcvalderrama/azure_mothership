d3e641f1b64de9a8b5021fe632eb608149b130cd
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-11-25 20:51:11 -0500

    add

--- production/build.ps1
+++ production/build.ps1
@@ -1,15 +1,26 @@
 # Install-Module -Name Az -AllowClobber -Scope CurrentUser
-# Import-Module Az.Resources
+
+$clientId = ($env:credentials | ConvertFrom-Json).clientId
+$clientSecret = ($env:credentials | ConvertFrom-Json).clientSecret | ConvertTo-SecureString -AsPlainText -Force
+$tenantId = ($env:credentials | ConvertFrom-Json).tenantId
+$credentials = New-Object System.Management.Automation.PSCredential($clientId, $clientSecret)
+
+$connected = Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+
 $params = @{
     vaults_secrets_name = "mothershipprodsecret"
     registries_registry_name = "mothershipprodregistry"
     userAssignedIdentities_identity_name = "mothershipappidentity"
 }
 
-# Connect-AzAccount
-
-# Set-AzContext 652129d9-5c47-49f8-ba58-0137a903edb0
 $resourceName = "mothership-rg"
 $location = "eastus"
 New-AzResourceGroup -Name $resourceName -Location $location -Force
 
+New-AzResourceGroupDeployment `
+-Mode Complete `
+-Name mothershipProdDeployment `
+-ResourceGroupName $resourceName `
+-TemplateFile ./template/template.json `
+-TemplateParameterObject $params `
+-Force
